27830572c9d425c9f38665195e0afe17754cb10f
shilpa4103 <shilpashree4103@gmail.com>
2024-08-18 17:36:28 +0530

    Education done

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -8,6 +8,20 @@ const Education = () => {
         <h1>Education</h1>
         <img src={theme_pattern} alt=""/>
       </div>
+      <div className="education-container">
+      <div className='education-box'>
+      <h1>JSS Science and Technology University (Formerly SJCE), Mysuru</h1>
+      <h2>Bachelor of Engineering  (2022-2025)</h2>
+      <h3>Computer Science</h3>
+      <h3>8.95</h3>
+      </div>
+      <div className='education-box'>
+      <h1>Jawahar Navodaya Vidyalaya, Chickamagaluru</h1>
+      <h2>XII CBSE (2014-2021) </h2>
+      <h3>PCMB</h3>
+      <h3>96.5%</h3>
+      </div>
+      </div>
     </div>
   )
 }
